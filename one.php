8d0491fa4519161519a8c7d7ab2098c32686b55a
William Lumbantobing <william.sl.tobing@gmail.com>
2021-11-30 19:00:42 +0700

    add solution number one

--- one.php
+++ one.php
@@ -1,1 +1,26 @@
+<?php
+$data = array(
+    0 => array(
+        'id' => 1,
+        'name' => 'Foo',
+        'image' => array('url' => '/pub/foo.jpg'),
+    ),
+    1 => array(
+        'id' => 2,
+        'name' => 'Bar',
+        'image' => array('url' => '/pub/bar.jpg'),
+    ),
+    2 => array(
+        'id' => 3,
+        'name' => 'Foo Bar',
+        'image' => array('url' => '/pub/foobar.jpg'),
+    ),
+);
 
+$result = array();
+
+foreach ($data as $a) {
+    array_push($result, $a['image']['url']);
+}
+
+print_r($result);
